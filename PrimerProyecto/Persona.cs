3a4f4c6d12a0741e8c9c844881f5e2650efa18dc
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-13 08:14:41 -0700

    Agregar archivos de proyecto.

---
+++ PrimerProyecto/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerProyecto
+{
+    class Persona
+    {
+        public int g_Edad { get; set; }
+        public string g_Nombre { get; set; }
+
+        public Persona(int m_edad, string m_nombre)
+        {
+            g_Edad = m_edad;                // Con get y set, ahora la variable g_Edad (En mayúscula por que se
+            g_Nombre = m_nombre;            // considera como un método) se acepta en los constructores sin "this".
+        }
+
+        public Persona()
+        {
+            g_Edad = 0;                     // "this" hace referencia a la edad de la clase; 
+            g_Nombre = "";                  //  la edad sin "this" es la edad del constructor
+        }
+
+        public void cumplirAño()
+        {
+            g_Edad++;
+        }
+    }
+}
---
+++ PrimerProyecto/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona(22, "José");
+
+            Console.WriteLine(persona1.g_Nombre + " tiene " + persona1.g_Edad + " años");
+            
+            persona1.cumplirAño();
+
+            Console.WriteLine("Ahora " + persona1.g_Nombre + " tiene " + persona1.g_Edad + " años");
+
+            Console.ReadLine();
+        }
+    }
+}
